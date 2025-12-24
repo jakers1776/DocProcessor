2c1702644879bba10601ddd97520234524a564d3
jakers821 <jakers1776@zoho.com>
2025-12-23 16:02:56 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,6 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/document-processor-ollama",
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires=">=3.7",
     install_requires=[
         "python-docx>=0.8.11",
